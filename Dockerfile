c09524ef2084f47b891889b3a7ec9c52b7203946
tsan <tsanyip@hotmail.com>
2021-12-11 19:46:23 +0100

    exchanger has to be created manually

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM jboss/keycloak:latest
 COPY ./target/keycloak-to-rabbit-2.1.jar /opt/jboss/keycloak/standalone/deployments/
+COPY ./utils/cli/KEYCLOAK_TO_RABBIT.cli /opt/jboss/startup-scripts/
 
 ENTRYPOINT ["/opt/jboss/tools/docker-entrypoint.sh"]
 
---
+++ src/main/java/com/github/aznamier/keycloak/event/provider/MessagingConfig.java
@@ -0,0 +1,7 @@
+package com.github.aznamier.keycloak.event.provider;
+
+public class MessagingConfig {
+    public static final String topicExchangeName = "leagueshare-exchanger";
+    public static final String queueName = "user-queue";
+    public static final String routingKey = "leagueshare-key";
+}
